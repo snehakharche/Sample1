e7cfb0676b2c7bd661725119d3d0f0a0335c5601
Sneha <snehakharche7@gmail.com>
2019-08-28 16:10:28 +0530

    Hello first commit

---
+++ Addition/src/a1/Add.java
@@ -0,0 +1,8 @@
+package a1;
+
+public class Add {
+
+	public static void main(String[] args) {
+		System.out.println("Hey there fitnesse");
+	}
+}
---
+++ Addition/src/a1/AddNumbers.java
@@ -0,0 +1,24 @@
+package a1;
+
+public class AddNumbers {
+	int num1;
+	int num2;
+
+	public void setNum1(int num) {
+		this.num1 = num;
+	}
+
+	public void setNum2(int num) {
+		this.num2 = num;
+	}
+
+	public int add() {
+		// int a;
+
+		// a = num1 + num2;
+		return num1 + num2;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
